621762b7d74aa161f1025d81e062b0c76015e10c
almamunrasel <raselalmamun096@gmail.com>
2026-04-08 17:40:58 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 const dotenv=require('dotenv');
+const connectDB =require('./src/config/db');
 
 
 
@@ -11,6 +12,8 @@ const app=express();
 
 
 app.use(express.json());
+connectDB();
+
 
 app.get('/',(req,res)=>{
   res.send('fintech server is on');
