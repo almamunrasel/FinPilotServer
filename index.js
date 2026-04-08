71255a88f0f3ec87e0df0e03aa52c542b12c13f6
almamunrasel <raselalmamun096@gmail.com>
2026-04-08 16:46:26 +0600

    dependencies are added

---
+++ index.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const cors=require('cors');
+const dotenv=require('dotenv');
+
+
+
+dotenv.config();
+const PORT=process.env.PORT ||3001
+const app=express();
+
+
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+  res.send('fintech server is on');
+})
+app.use((err,req,res,next)=>{
+  console.error(err.stack);
+  res.status(500).json({
+    message:"there is an error in server"
+  })
+})
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+
+})
